87782c87c99819d21dd7a886b35fc99b2792e6cf
Beliewing <karina25.du@hotmail.fr>
2021-11-20 23:03:05 +0100

    test put

--- TP-HTTP-Code/src/http/server/WebServer.java
+++ TP-HTTP-Code/src/http/server/WebServer.java
@@ -3,8 +3,10 @@
 package http.server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -27,6 +29,7 @@ public class WebServer {
 	/**
 	 * WebServer constructor.
 	 */
+		
 	protected void start() {
 		ServerSocket s;
 
@@ -55,47 +58,73 @@ public class WebServer {
 				// blank line signals the end of the client HTTP
 				// headers.
 				String str = ".";
-				while (str != null && !str.equals("")) {
+				String typeRequest = "";
+				File fileCreated = null;
+				boolean body = false;
+				boolean read = false;
+				while ((str != null && !str.equals("")) || body || read) {
+					System.out.println("read : " + read);
 					str = in.readLine();
-                    System.out.println("received : " + str);
-                    if(str != null && !str.equals("")) {
-                        String[] words = str.split(" ");
-                        for(String sx : words) {
-                            System.out.println(sx);
-                            if (words[0].toUpperCase().equals("GET")) {
-                            	if (words[1].contains("text")) {
-                            		String name = words[1].substring(words[1].lastIndexOf("/") + 1);
-                            		displayText(out,name);
-                            	}else if (words[1].contains("html")){
-                            		String name = words[1].substring(words[1].lastIndexOf("/") + 1);
-                            		displayHtml(out,name);
-                            	}else if (words[1].equals("/")){
-                            		displayIndex(out);
-                            	}else {
-                            		displayBadRequest(out);
-                            	}
-                            	break;
-                            }else if (words[0].toUpperCase().equals("DELETE")) {
-                            	String name = words[1].substring(words[1].lastIndexOf("/") + 1);
-                            	File file = null;
-                            	if (words[1].contains("text")) {
-                                	file = new File("../ressources/"+name+".txt");
-                            	}else if (words[1].contains("html")){
-                                	file = new File("../ressources/"+name+".html");
-                            	} else {
-                            		displayBadRequest(out);
-                            	}
-                            	System.out.println("EXISTS"+file.exists());
-                            	if (file != null && file.delete()) {
-                            		System.out.println("success");
-                            		displayDelete(out);
-                            	} else {
-                            		System.out.println("fail");
-                            		displayNotFound(out);
-                            	}
-                            }
-                        }
-                    }
+					System.out.println("received : " + str);
+					if (str != null && !str.equals("")) {
+						String[] words = str.split(" ");
+						if (words[0].toUpperCase().equals("GET")) {
+							if (words[1].contains("text")) {
+								String name = words[1].substring(words[1].lastIndexOf("/") + 1);
+								displayText(out, name);
+							} else if (words[1].contains("html")) {
+								String name = words[1].substring(words[1].lastIndexOf("/") + 1);
+								displayHtml(out, name);
+							} else if (words[1].equals("/")) {
+								displayIndex(out);
+							} else {
+								displayBadRequest(out);
+							}
+						} else if (words[0].toUpperCase().equals("DELETE")) {
+							String name = words[1].substring(words[1].lastIndexOf("/") + 1);
+							File file = null;
+							if (words[1].contains("text")) {
+								file = new File("../ressources/" + name + ".txt");
+							} else if (words[1].contains("html")) {
+								file = new File("../ressources/" + name + ".html");
+							} else {
+								displayBadRequest(out);
+							}
+							System.out.println("EXISTS" + file.exists());
+							if (file != null && file.delete()) {
+								System.out.println("success");
+								displayDelete(out);
+							} else {
+								System.out.println("fail");
+								displayNotFound(out);
+							}
+						} else if (words[0].toUpperCase().equals("PUT")) {
+							typeRequest = "PUT";
+							body = true;
+							if (words[1].contains("text")) {
+								String name = words[1].substring(words[1].lastIndexOf("/") + 1);
+								fileCreated = putText(out, name);
+							} else if (words[1].contains("html")) {
+								String name = words[1].substring(words[1].lastIndexOf("/") + 1);
+								putHtml(out, name);
+							} else if (words[1].equals("/")) {
+								displayIndex(out);
+							} else {
+								displayBadRequest(out);
+							}
+						} else if (str == null || str.equals("")){
+							System.out.println("READ = TRUE");
+							read = true;
+							body = false;
+						} else if (read) {
+							if (str == null || str.equals("")){
+								read = false;
+							} else {
+								System.out.println("STRINNNNG:" + str);
+								addText(fileCreated,str);
+							}
+						}
+					}
 					System.out.println("String:" + str);
 				}
 				remote.close();
@@ -104,16 +133,16 @@ public class WebServer {
 			}
 		}
 	}
-	
+
 	public void displayIndex(PrintWriter out) {
 		out.println("HTTP/1.0 200 OK");
 		out.println("Content-Type: html");
 		out.println("Server: Bot");
-		out.println("");		                				
-    	out.println("<H1>Index</H1>");
+		out.println("");
+		out.println("<H1>Index</H1>");
 		out.flush();
 	}
-	
+
 	public void displayBadRequest(PrintWriter out) {
 		out.println("HTTP/1.0 500");
 		// this blank line signals the end of the headers
@@ -122,55 +151,117 @@ public class WebServer {
 		out.println("<H1>Bad request </H1>");
 		out.flush();
 	}
-	
+
 	public void displayNotFound(PrintWriter out) {
 		out.println("HTTP/1.0 404");
 		out.println("");
 		out.println("<H1>404 Not found</H1>");
 		out.flush();
 	}
-	
+
+	public void displayErrorCreate(PrintWriter out) {
+		out.println("HTTP/1.0 400");
+		out.println("");
+		out.println("<H1>400 Could not create file</H1>");
+		out.flush();
+	}
+
 	public void displayDelete(PrintWriter out) {
 		out.println("HTTP/1.0 200");
 		out.println("");
 		out.println("<H1>File deleted</H1>");
 		out.flush();
 	}
-	
+
 	public void displayText(PrintWriter out, String name) {
 		try {
-        	BufferedReader br=new BufferedReader(new FileReader("../ressources/"+name+".txt"));
-        	String line;
+			BufferedReader br = new BufferedReader(new FileReader("../ressources/" + name + ".txt"));
+			String line;
 			out.println("HTTP/1.0 200 OK");
 			out.println("Content-Type: text");
 			out.println("Server: Bot");
-			out.println("");		                				
-        	out.println("<H1>The text</H1>");
-        	while((line=br.readLine())!=null && line.length()!=0) {
-        		out.println(line);
-        		out.println("<br/>");
-        	}
+			out.println("");
+			out.println("<H1>The text</H1>");
+			while ((line = br.readLine()) != null && line.length() != 0) {
+				out.println(line);
+				out.println("<br/>");
+			}
 			out.flush();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			displayNotFound(out);
 		}
 	}
-	
+
 	public void displayHtml(PrintWriter out, String name) {
 		try {
-	    	BufferedReader br=new BufferedReader(new FileReader("../ressources/"+name+".html"));
-	    	String line;
+			BufferedReader br = new BufferedReader(new FileReader("../ressources/" + name + ".html"));
+			String line;
 			out.println("HTTP/1.0 200 OK");
 			out.println("Content-Type: html");
 			out.println("Server: Bot");
-			out.println("");		                				
-	    	out.println("<H1>The html</H1>");
-	    	while((line=br.readLine())!=null || line=="" ) {
-	    		out.println(line);
-	    	}
+			out.println("");
+			out.println("<H1>The html</H1>");
+			while ((line = br.readLine()) != null || line == "") {
+				out.println(line);
+			}
 			out.flush();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			displayNotFound(out);
+		}
+	}
+
+	public File putText(PrintWriter out, String name) {
+		File file = new File("../ressources/" + name + ".txt");
+		try {
+			if (file.createNewFile()) {
+				out.println("HTTP/1.0 201 OK");
+				out.println("Content-Type: text");
+				out.println("Server: Bot");
+				out.println("");
+				out.println("<H1>Text created</H1>");
+				out.flush();
+			} else {
+				PrintWriter writer = new PrintWriter(file);
+				writer.print("");
+				writer.close();
+				out.println("HTTP/1.0 204 OK");
+				out.println("Content-Type: text");
+				out.println("Server: Bot");
+				out.println("");
+				out.println("<H1>Text modified</H1>");
+				out.flush();
+			}
+		} catch (Exception e) {
+			displayErrorCreate(out);
+		}
+		return file;
+	}
+	
+	public void addText(File file, String newString) {
+		try {
+		    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		    writer.append(newString);
+		    writer.close();
+		}catch(Exception e) {
+		}
+	}
+
+
+	public void putHtml(PrintWriter out, String name) {
+		File fileName = new File("../ressources/" + name + ".html");
+		try {
+			if (fileName.createNewFile()) {
+				out.println("HTTP/1.0 201 OK");
+				out.println("Content-Type: html");
+				out.println("Server: Bot");
+				out.println("");
+				out.println("<H1>Text created</H1>");
+				out.flush();
+			} else {
+				displayErrorCreate(out);
+			}
+		} catch (Exception e) {
+			displayErrorCreate(out);
 		}
 	}
 
